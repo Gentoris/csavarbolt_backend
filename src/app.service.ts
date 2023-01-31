02767c72cbc17fc6ca98ed04b1152110b1771df9
Kerecsényi Márk <karatelovag@gmail.com>
2023-01-31 10:54:40 +0100

    javításra szoruló rendelés POST

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { Order } from './order.entity';
 import { Screw } from './screw.entity';
 
 @Injectable()
 export class ScrewService {
+  createOrder(order: any) {
+    throw new Error('Method not implemented.');
+  }
+  findOne(screwId: number) {
+    throw new Error('Method not implemented.');
+  }
+  update(screwId: number, screw: any) {
+    throw new Error('Method not implemented.');
+  }
+  placeOrder(screwId: number, order: Order): import("./order.entity").Order | PromiseLike<import("./order.entity").Order> {
+    throw new Error('Method not implemented.');
+  }
   delete(id: number) {
     throw new Error('Method not implemented.');
   }
